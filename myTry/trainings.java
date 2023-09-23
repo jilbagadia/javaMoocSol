28d3dd72c2a463810cb5ea950119087427a5b750
jilb <bagadiajb@gmail.com>
2023-09-23 20:07:49 +0530

    made changes in trainings file

--- myTry/trainings.java
+++ myTry/trainings.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class trainings {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Lokesh \n" + "Jil");
+		System.out.println("Hello Bro \n" + "Jil");
 		
         LinkedList<String> first=new LinkedList<String>();
         LinkedList<String> second=new LinkedList<String>();
@@ -37,6 +37,21 @@ public class trainings {
         int number=Integer.valueOf(scan.nextLine());
         System.out.println("You gave the number "+Integer.valueOf(2+number));
         System.out.println("You gave the number "+ (number+3));
-	}
+        int i,m=0,flag=0;      
+    	int n=3;    
+    	m=n/2;      
+    	if(n==0||n==1){  
+    	   System.out.println(n+" is not prime number");      
+    	  }else{  
+    	   for(i=2;i<=m;i++){      
+    	    if(n%i==0){      
+    	     System.out.println(n+" is not prime number");      
+    	     flag=1;      
+    	     break;      
+    	    }      
+    	   }      
+    	   if(flag==0)  { System.out.println(n+" is prime number"); }  
+    	  }
+    }
 
 }
